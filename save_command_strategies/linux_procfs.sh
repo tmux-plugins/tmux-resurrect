2382467b8e74851c719ec71199ab65fd1104d772
*Kim Zick <k@9k1.us>
2019-12-17 11:04:45 -0500

    Change `CPID` to something a little less cryptic

--- save_command_strategies/linux_procfs.sh
+++ save_command_strategies/linux_procfs.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PANE_PID="$1"
-CPID=$(pgrep -P $PANE_PID)
+COMMAND_PID=$(pgrep -P $PANE_PID)
 
 exit_safely_if_empty_ppid() {
 	if [ -z "$PANE_PID" ]; then
@@ -12,8 +12,8 @@ exit_safely_if_empty_ppid() {
 }
 
 full_command() {
-	[[ -z "$CPID" ]] && exit 0
-	cat /proc/${CPID}/cmdline | xargs -0 printf "%q "
+	[[ -z "$COMMAND_PID" ]] && exit 0
+	cat /proc/${COMMAND_PID}/cmdline | xargs -0 printf "%q "
 }
 
 main() {
